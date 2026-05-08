2d014facb7833993f145edc119bdfd3af2f47c89
jeffdosantos <jeff.fbds@gmail.com>
2026-05-08 17:45:21 -0300

    Update config.js

--- config.js
+++ config.js
@@ -2,5 +2,5 @@
 // 2) Copie estes dados em Supabase > Project Settings > API
 // 3) Use somente a chave publica anon/publishable. Nunca use a service_role no front-end.
 
-export const SUPABASE_URL = "https://SEU-PROJETO.supabase.co";
-export const SUPABASE_ANON_KEY = "SUA_CHAVE_PUBLICA_ANON_OU_PUBLISHABLE";
+export const SUPABASE_URL = "https://isjhrccbvgznzecqhwom.supabase.co/rest/v1/";
+export const SUPABASE_ANON_KEY = "sb_publishable_xbUUw5xAwXfIz2WxXqZPzw_GUxOfhkcE";
